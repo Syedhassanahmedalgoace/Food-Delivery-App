d644910eec43f11178dc93f5a947516cf1d36ca8
Syedhassanahmedalgoace <hassan.algoace@gmail.com>
2025-03-14 11:28:26 +0500

    react js

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officiis
+          numquam optio repellendus quo libero nostrum, saepe doloremque
+          perferendis. Unde perferendis dolor voluptatibus veniam illo?
+          Accusamus nesciunt minima possimus non?
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
